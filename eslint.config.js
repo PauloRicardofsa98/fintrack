d383687e249e2e0f023a81baf2d903ceb6ba17db
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-13 17:19:10 -0300

    chore: add shadcn setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import js from "@eslint/js";
-import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
 export default [
   { ignores: ["dist"] },
@@ -31,12 +31,9 @@ export default [
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react/jsx-no-target-blank": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "react/prop-types": "off",
     },
   },
 ];
